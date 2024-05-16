8bcb879ae8de2e31ced41759425dd7915777e5fa
koechMR <122898923+koechMR@users.noreply.github.com>
2024-05-16 07:56:16 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,18 +3,13 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Dashboard from './views/Dashboard'
 
-
-
-
 function App() {
   return (
     <Router>
       <AuthProvider>
         < Navbar/>
-        <Switch>
-          <PrivateRoute component={Dashboard} path="/dashboard" exact />
-          
-        </Switch>
+        
+          <PrivateRoute component={Dashboard} path="/dashboard" exact /
       </AuthProvider>
     </Router>
   )
