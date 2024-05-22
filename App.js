371ae1f7ad02a6f15213fee65b3bda2ba4d713d1
koechMR <122898923+koechMR@users.noreply.github.com>
2024-05-22 19:20:28 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,2 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-
-
-function App() {
-  return (
-    <Router>
-      
-    </Router>
-  )
-}
-
-export default App
